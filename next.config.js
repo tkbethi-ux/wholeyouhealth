0549477a5757f0ca97be62818dde5227f7ae8e73
BETHI Tharun Kumar <tharun.kumar.bethi@hexagon.com>
2025-11-06 14:47:07 -0500

    disable ligingcss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: false, // disables lightningcss
+  },
   images: {
     unoptimized: true,
   },
